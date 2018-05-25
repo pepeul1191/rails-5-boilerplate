35fc3a9cfe69ca728d6e0128cb0c322d1586bb3e
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-24 23:05:35 -0500

    Login con estilos

--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -6,8 +6,8 @@ module LoginHelper
         'bower_components/bootstrap/dist/css/bootstrap.min',
         'bower_components/font-awesome/css/font-awesome.min',
         'bower_components/swp-backbone/assets/css/constants',
-        'bower_components/swp-backbone/assets/css/dashboard',
-        'bower_components/swp-backbone/assets/css/table',
+        'bower_components/swp-backbone/assets/css/login',
+        'assets/css/login',
       ]
     else
       rpta = [
@@ -23,13 +23,7 @@ module LoginHelper
       rpta = [
         'bower_components/jquery/dist/jquery.min',
         'bower_components/bootstrap/dist/js/bootstrap.min',
-        'bower_components/underscore/underscore-min',
-        'bower_components/backbone/backbone-min',
-        'bower_components/backbone.marionette/lib/backbone.marionette.min',
-        'bower_components/handlebars/handlebars.min',
-        'bower_components/swp-backbone/layouts/application',
-        'bower_components/swp-backbone/views/table',
-        'bower_components/swp-backbone/views/modal',
+        'assets/js/login',
       ]
     else
       rpta = [
