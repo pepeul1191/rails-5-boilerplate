d42e8ce0e155e173d7d481231bb3fdb833670876
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-25 08:18:29 -0500

    Migración, ubicaciones crear libros

---
+++ db/migrations_archivos/005_crear_libros.rb
@@ -0,0 +1,20 @@
+require 'sequel'
+
+Sequel.migration do
+  up do
+    create_table(:libros) do
+      primary_key :id
+      String :nombre, null: false, size: 45
+      Integer :paginas, null: false
+    end
+
+    alter_table(:libros) do
+      add_foreign_key :extension_id, :extensiones
+    end
+	end
+
+  down do
+    drop_column :libros, :extension_id
+    drop_table(:libros)
+	end
+end
