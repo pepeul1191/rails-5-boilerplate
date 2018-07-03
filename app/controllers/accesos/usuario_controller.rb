6f3d6319c88dc575305100f925a001e2fb47981c
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-03 18:53:45 -0500

    Validacr contraseña antigua (Accesos

--- app/controllers/accesos/usuario_controller.rb
+++ app/controllers/accesos/usuario_controller.rb
@@ -52,4 +52,33 @@ class Accesos::UsuarioController < ApplicationController
 		end
     render :plain => rpta, :status => status
   end
+
+  def contrasenia_repetida
+    rpta = nil
+    status = 200
+    begin
+      r = HTTParty.post(
+        CONSTANTS[:servicios][:accesos][:url] + 'usuario/contrasenia_repetida',
+        headers:{
+          CONSTANTS[:servicios][:accesos][:csrf_key] => CONSTANTS[:servicios][:accesos][:csrf_value],
+          'Content-Type' => 'application/x-www-form-urlencoded',
+          'charset' => 'utf-8'
+        },
+        body:{
+          :data => params[:data],
+        })
+      status = r.code
+      rpta = r.body
+		rescue Exception => e
+      rpta = {
+				:tipo_mensaje => 'error',
+				:mensaje => [
+					'Se ha producido un error realizar validar la contrasenia_repetida del servicio',
+					e.message
+				]
+			}.to_json
+			status = 500
+		end
+    render :plain => rpta, :status => status
+  end
 end
